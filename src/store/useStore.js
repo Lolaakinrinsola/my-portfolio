39017b92d6f05ca2280de14b44ef14adae2f6c02
ololade <ololadeakinrinsola@gmail.com>
2024-11-26 11:01:52 +0100

    local storage

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,23 +1,33 @@
 import { create } from 'zustand';
 const services = [
-    "Development",
-    "Design",
-    "Prototyping",
-    "Optimization",
-    "Testing",
-    "Consulting",
-    "Maintenance",
-    "Integration",
-    "Deployment",
-    "Debugging",
-  ];
-const useStore = create((set,get) => ({
-  isDarkMode: true, // Default is light mode
-  space:'px-[15px] md:px-[5%] lg:px-[10%] dark:bg-dark bg-light',
-  services:services,
-  toggleTheme: () =>{ 
-    const isDarkMode=get().isDarkMode
-    set({ isDarkMode: !isDarkMode })},
+  "Development",
+  "Design",
+  "Prototyping",
+  "Optimization",
+  "Testing",
+  "Consulting",
+  "Maintenance",
+  "Integration",
+  "Deployment",
+  "Debugging",
+];
+
+// Initialize the theme from localStorage or default to light mode
+const getInitialDarkMode = () => {
+  const storedTheme = localStorage.getItem('isDarkMode');
+  return storedTheme ? JSON.parse(storedTheme) : false; // Default is false (light mode)
+};
+
+const useStore = create((set, get) => ({
+  isDarkMode: getInitialDarkMode(),
+  space: 'px-[15px] md:px-[5%] lg:px-[10%] ',
+  services: services,
+  toggleTheme: () => {
+    const currentMode = get().isDarkMode;
+    const newMode = !currentMode;
+    set({ isDarkMode: newMode });
+    localStorage.setItem('isDarkMode', JSON.stringify(newMode)); // Sync with localStorage
+  },
 }));
 
 export default useStore;
